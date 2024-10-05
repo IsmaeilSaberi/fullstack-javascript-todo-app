791de35cd51542f6b1351e34c9c6b8df1129a434
Ismaeil Saberi <ismaeilsaberi@gmail.com>
2024-10-04 20:25:43 -0700

    creating the main structure of html for dispalying todos with java script

--- frontend/script.js
+++ frontend/script.js
@@ -19,6 +19,46 @@ async function get_todos() {
 function display_todos(todoArr) {
   todoArr.forEach((todoElem) => {
     console.log(todoElem);
+
+    // Parent element for display todos
+    let todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    // Childrens for displaying todo info
+    let todoInfo = document.createElement("div");
+    todoInfo.classList.add("todo-info");
+    let todoBtn = document.createElement("form");
+    todoBtn.classList.add("todo-btn");
+
+    // Grand children for displaying todos
+    let todoCompleted = document.createElement("input");
+    todoCompleted.classList.add("todo-completed");
+    todoCompleted.setAttribute("type", "checkbox");
+    todoCompleted.checked = todoElem.completed;
+    let todoName = document.createElement("p");
+    todoName.classList.add("todo-name");
+    todoName.innerHTML = todoElem.name;
+
+    // Buttons of todo element
+    let todoEdit = document.createElement("button");
+    todoName.classList.add("todo-edit");
+    todoEdit.innerHTML = "Edit";
+    todoEdit.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("Open Model");
+    });
+    let todoDel = document.createElement("button");
+    todoName.classList.add("todo-delete");
+    todoEdit.innerHTML = "Delete";
+    todoEdit.addEventListener("click", (e) => {
+      e.preventDefault();
+      console.log("Delete Model");
+    });
+
+    todoInfo.appendChild(todoCompleted);
+    todoInfo.appendChild(todoName);
+    todoInfo.appendChild(todoEdit);
+    todoInfo.appendChild(todoDel);
   });
 }
 
